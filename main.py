8804de66fe7e4bb431ce571e0e820cf3f0a80ca3
danilo <solo2danny@gmail.com>
2022-08-07 23:18:17 -0400

    agrego token-agrego apitoplevel

--- main.py
+++ main.py
@@ -26,5 +26,24 @@ def obtener_token(usuario, clave):
     respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
     token = respuesta.json()["imdata"][0]["aaaLogin"]["attributes"]["token"]
     return token
-token = obtener_token(conf.usuario, conf.clave)
-print(token)
+obtener_token(conf.usuario, conf.clave)
+
+
+##GET http://apic-ip-address/api/class/topSystem.json
+def top_system():
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    lagalleta = {
+        "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
+    }
+
+    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies=lagalleta, verify=False)
+    for i in range(0, 3):
+    #respuesta.json()["imdata"][0]["topSystem"]["attributes"]["state"]
+    #se cambia el 0 por la variable i que indica que tiene el valor del 1 al 3 segun lo definido
+    #respuesta.json()["imdata"][i]["topSystem"]["attributes"]["state"]
+
+         estado = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["state"]
+         print(estado)
+top_system()
